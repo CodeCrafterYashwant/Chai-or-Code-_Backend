542a1b24041e8e546944a3b98bdad7d1cb08d4d2
CodeCrafterYashwant <yashnamdevnwg@gmail.com>
2026-04-05 20:28:09 +0530

    Some Changes

--- src/index.js
+++ src/index.js
@@ -2,20 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db/index.js";
 
-connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB();
 
 /*import express from "express";
 const app = express(); 
