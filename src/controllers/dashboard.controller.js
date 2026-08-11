9180d7eed8e8b0123c6a01584c7b9280755afa40
CodeCrafterYashwant <yashnamdevnwg@gmail.com>
2026-08-11 16:49:49 +0530

    Implement video retrieval logic for channel with response handling

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -11,7 +11,16 @@ const getChannelStats = asyncHandler(async (req, res) => {
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+    const channelId = req.user?._id
+    const videos = await Video.find({
+        owner:channelId
+    }).sort({createdAt:-1})
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            videos,
+            videos.length === 0 ? "Channel has no Video Yet." : 'All videos of Channel Feacthed Successfully.')
+    )
 })
 
 export {
